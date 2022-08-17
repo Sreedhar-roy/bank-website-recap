0cebd7decbacec875da14c3f33836ffbc7687903
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-17 15:55:06 +0600

    project

--- js-script/widthdrow.js
+++ js-script/widthdrow.js
@@ -1,10 +1,26 @@
 // Step 1
-document.getElementById('btn-Withdrow').addEventListener('click', function(){
+document.getElementById('btn-Withdrow').addEventListener('click', function () {
     // step 2
-    const widthdroField = document.getElementById('btn-Withdrow');
-    const totalWidthdrowString = widthdroField.value;
-    const newWidthdrowField = parseFloat(totalWidthdrowString);
-
+    const widthdrowField = document.getElementById('widthdrow-field');
+    const newWidthdrowFieldString = widthdrowField.value;
+    const newWidthdrowField = parseFloat(newWidthdrowFieldString);
     // step 3
-    widthdroField.value= '';
+    widthdrowField.value = '';
+    // step 4
+    const widthdrowToral = document.getElementById('withdrow-tatal');
+    const widthdrowToralString = widthdrowToral.innerText;
+    const newWidthdrowTotal = parseFloat(widthdrowToralString);
+    // steap 5
+    const newWidthdrowTotalElement = newWidthdrowTotal + newWidthdrowField;
+    widthdrowToral.innerText = newWidthdrowTotalElement;
+
+    // step 6
+    const balanceElement = document.getElementById('Balance-tatal');
+    const previousnewTotalbalance = balanceElement.innerText;
+    const CurrentTotalBalance = parseFloat('previousnewTotalbalance');
+
+    // step 7
+
+    const NewBalance = CurrentTotalBalance - newWidthdrowField;
+    balanceElement.innerText = NewBalanceTotal;
 })
